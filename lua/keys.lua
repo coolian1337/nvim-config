c642c395551d335b17bd01d72ed212aae4f0890d
coolian1337 <julian.wogersien@gmail.com>
2023-01-13 09:20:30 +0100

    fixed that one issue

--- lua/keys.lua
+++ lua/keys.lua
@@ -1,4 +1,15 @@
 
+local function map(mode, lhs, rhs, opts)
+    local options = { noremap = true, silent = true }
+    if opts then
+        if opts.desc then
+            opts.desc = "keymaps.lua: " .. opts.desc
+        end
+        options = vim.tbl_extend('force', options, opts)
+    end
+    vim.keymap.set(mode, lhs, rhs, options)
+end
+
 -- Vimspector
 vim.cmd([[
 nmap <F9> <cmd>call vimspector#Launch()<cr>
